ec27d13040fdcb0d9ba443f1501ae69d9f402360
KimCuong236 <23kimcuong@gmail.com>
2025-07-25 16:20:19 +0700

    layout

--- src/pages/admin/manager-user/ManagetUserPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const ManagetUserPage = (props: Props) => {
-  return <div>ManagetUserPage</div>;
-};
-
-export default ManagetUserPage;
--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,18 +1,27 @@
 import { RouteObject } from "react-router-dom";
-import DashBoardAdmin from "../pages/admin/DashBoardAdmin";
-import ManagetUserPage from "../pages/admin/manager-user/ManagetUserPage";
+
+import ManagerUserPage from "../pages/admin/manager-user/ManagerUserPage";
 import ManagerClassPage from "../pages/admin/manager-class/ManagerClassPage";
 import ManagerMajorPage from "../pages/admin/manager-major/ManagerMajorPage";
 import ManagerSubjectPage from "../pages/admin/manager-subject/ManagerSubjectPage";
+import ProtectedRoute from "./ProtectedRoute";
+import { RoleEnum } from "../common/types";
+import AdminLayout from "../components/layouts/AdminLayout";
 
 const adminRoutes: RouteObject[] = [
   {
-    path: "/",
-    element: <DashBoardAdmin />,
+    path: "/admin",
+    element: (
+      <ProtectedRoute allowedRoles={[RoleEnum.SUPER_ADMIN]}>
+        <AdminLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      { path: "users", element: <ManagerUserPage /> },
+      { path: "classes", element: <ManagerClassPage /> },
+      { path: "majors", element: <ManagerMajorPage /> },
+      { path: "subjects", element: <ManagerSubjectPage /> },
+    ],
   },
-  { path: "/users", element: <ManagetUserPage /> },
-  { path: "/classes", element: <ManagerClassPage /> },
-  { path: "/majors", element: <ManagerMajorPage /> },
-  { path: "/subjects", element: <ManagerSubjectPage /> },
 ];
 export default adminRoutes;
