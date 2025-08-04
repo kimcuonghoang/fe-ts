7f636ff535154a6e862bb28d11e6c8b28a9d9890
KimCuong236 <23kimcuong@gmail.com>
2025-08-04 09:56:30 +0700

    api subject

---
+++ src/common/api/subjectApi.ts
@@ -0,0 +1,34 @@
+import api from "./index";
+
+export type Subject = {
+  _id: string;
+  name: string;
+  englishName: string;
+  code: string;
+  description: string;
+  deletedAt?: Date | null;
+};
+
+export const getSubject = async (): Promise<Subject[]> => {
+  const res = await api.get("/subjects");
+  return res.data.data;
+};
+
+export const createSubject = async (data: Omit<Subject, "_id">) => {
+  const res = await api.post("/subjects", data);
+  return res.data;
+};
+
+export const updateSubject = async (id: string, data: Omit<Subject, "_id">) => {
+  const res = await api.patch(`/subjects/${id}`, data);
+  return res.data;
+};
+
+export const deleteSubject = async (id: string) => {
+  const res = await api.delete(`/subjects/${id}`);
+  return res.data;
+};
+export const restoreSubject = async (id: string) => {
+  const res = await api.patch(`/subjects/restore/${id}`);
+  return res.data;
+};
